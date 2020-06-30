07cf7d9bdb6763b665ca28d3e28d26c1f1a2956f
Scott Bromander <scottbromander@gmail.com>
2020-06-30 11:41:06 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const greeting = 'Hello from Node!';
+const someNumber = 3;
+const people = require('./people');
+
+people();
+
+const scottsDog = {
+  name: 'baxter',
+};
+
+console.log(greeting, someNumber);
+console.log(scottsDog.name);
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Scott', 'Myron', 'Rachael'];
+
+function logPeople() {
+  for (let person of people) {
+    console.log(person);
+  }
+}
+
+module.exports = logPeople;
